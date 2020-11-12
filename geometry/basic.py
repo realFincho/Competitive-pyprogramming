d4cadabb520c715c614ca94e4c4818cddb1f37ed
Fincho <rlog58@ya.ru>
2020-11-12 15:39:34 +0300

    add basic geometry

---
+++ geometry/basic.py
@@ -0,0 +1,18 @@
+def dot2D(v1, v2):
+    """
+
+    :param v1:
+    :param v2:
+    :return:
+    """
+    return sum(c1 * c2 for c1, c2 in zip(v1, v2))
+
+def dot3D(v1, v2):
+    """
+
+    :param v1:
+    :param v2:
+    :return:
+    """
+    return sum(c1 * c2 for c1, c2 in zip(v1, v2))
+
